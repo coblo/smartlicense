2dd8764b7eeb1b843349cf6866f931e34442f80a
Titusz Pan <titusz.pan@gmail.com>
2018-04-03 18:21:22 +0200

    add preregistered image to demo content

--- smartlicense/management/commands/import_demo_content.py
+++ smartlicense/management/commands/import_demo_content.py
@@ -15,7 +15,9 @@ class Command(BaseCommand):
         path = join(settings.BASE_DIR, 'smartlicense', 'fixtures', fname)
         print('Import demo content:', path)
         MediaContent.objects.get_or_create(
+            ident='CCGHPBbk4QdBe-CYDfTq7Qc7Fre-CDh996KkRgC34-CRLdd9g4BSUyY',
             title="Image of Cat",
             name=fname,
-            file=UploadedFile(open(path, 'rb'))
+            file=UploadedFile(open(path, 'rb')),
+            txid='60398ad4534713ae66e179f70fc06d0f7d76d15fbef840a319b1ee4a041704ad'
         )
